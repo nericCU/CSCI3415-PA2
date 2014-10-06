50f7f0e6b7cf75080b0019c74fd68b0a37f7e3a9
Eric <eric.nguyen@ucdenver.edu>
2014-10-06 15:04:59 -0600

    Added example Ada code and compile instructions

---
+++ HandsOuts/HelloWorldSample/hello.adb
@@ -0,0 +1,9 @@
+-- //Example of Hello World for ADA
+
+with ada.text_io;   -- Tell compiler to use i/o library
+use  ada.text_io;   -- Use library routines w/o fully qualified names
+
+procedure hello is
+begin
+        put("Hello World!");
+end hello; 
