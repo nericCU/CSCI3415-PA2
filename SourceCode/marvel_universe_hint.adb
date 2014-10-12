32bc8c0986597fb63a5c280ea3dad8acd98eb92b
Paul Rodriguez <paul.rodriguez@ucdenver.edu>
2014-10-12 15:44:13 -0600

    Commented Code

--- SourceCode/marvel_universe_hint.adb
+++ SourceCode/marvel_universe_hint.adb
@@ -15,7 +15,7 @@ procedure Marvel_Universe_Hint is
 
   --Declaration of Buffer String used to read input file
   Buffer : String (1 .. 100); -- This should be sufficient for us.
-  Last : Natural;
+  Last : Natural; -- Used for determining the last element of a string
   
   --Declaration of variables to keep track of Number of Vertices and Number of Characters
   N_Vertices : Positive;
@@ -26,12 +26,14 @@ procedure Marvel_Universe_Hint is
   type Vertex_Name_Array is array (Positive range <>) of String_Access;
   type Vertex_Name_Array_Access is access Vertex_Name_Array;
 
+  --forward declaration
   Vertex_Names : Vertex_Name_Array_Access;
 
   --Declaration of Edge Matrix
   type Edge_Matrix is array (Positive range <>, Positive range <>) of Boolean;
-  type Edge_Matrix_Access is access Edge_Matrix;
+  type Edge_Matrix_Access is access Edge_Matrix;	
 
+   --forward declaration
   Edges : Edge_Matrix_Access;
 
   --Declaration of Vector used to keep count of
@@ -41,10 +43,16 @@ procedure Marvel_Universe_Hint is
   type Collaboration_Matrix is array (Positive range <>, Positive range <>) of Natural;
   type Collaboration_Matrix_Access is access Collaboration_Matrix;
 
+   --forward declaration
   Collaborations : Collaboration_Matrix_Access;
 
   --Implementation of Print_Statistics
+  --Pre: takes a array of positive integers and a string
+  --Post: displays minimum, maximum and standard deviation
+  --Notes:
   procedure Print_Statistics (Counts : Count_Vector; Title : String) is
+  
+	--declaration 
     Minimum_Index : Positive := Counts'First;
     Minimum : Natural := Counts (Counts'First);
     Maximum_Index : Positive := Counts'First;
@@ -53,6 +61,8 @@ procedure Marvel_Universe_Hint is
     Sum : Float := 0.0;
     Sum_Squares : Float := 0.0;
   begin
+  
+	-- finds max and min value within the array and their position within the array.
     for I in Counts'Range loop
       if Counts (I) < Minimum then
         Minimum_Index := I;
@@ -62,20 +72,25 @@ procedure Marvel_Universe_Hint is
         Maximum_Index := I;
         Maximum := Counts (I);
       end if;
+	  
+	  --sub-procedure
       declare
         X : Float := Float (Counts (I));
       begin
-        N := N + 1;
-        Sum := Sum + X;
-        Sum_Squares := Sum_Squares + X * X;
-      end;
+        N := N + 1;								--calculates number of iterations within count array
+        Sum := Sum + X;							--calculates the sum of the values within count array
+        Sum_Squares := Sum_Squares + X * X;		--calculates the squared sum of the values within count array
+      end; --end sub-procedure
     end loop;
+	
+	--displays results
     Put_Line ("The minimum " & Title & " is " &
               Natural'Image (Minimum) & " (" &
-              Vertex_Names (Minimum_Index).all & ").");
+              Vertex_Names (Minimum_Index).all & ").");  --maps Minimum_Index to Vertex_Names to display character or comic book name
     Put_Line ("The maximum " & Title & " is " &
               Natural'Image (Maximum) & " (" &
-              Vertex_Names (Maximum_Index).all & ").");
+              Vertex_Names (Maximum_Index).all & ").");	 --maps Maximum_Index to Vertex_Names to display character or comic book name
+	--Calculates and displays average and standard deviation from sub-procedure.
     Put ("The average " & Title & " is ");
     Put (Sum / (Float (N)), Fore => 1, Aft => 2, Exp => 0);
     Put_Line (".");
@@ -92,22 +107,25 @@ begin
   Put_Line ("File " & Input_File_Name & " opened for input.");
 
   -- Read and parse the *Vertices line.
+  --sub-procedure
   declare
     Matches : Match_Array (0 .. 2);
-    Vertices_Regexp : constant string := "^\*Vertices (\d+) (\d+)$";
+    Vertices_Regexp : constant string := "^\*Vertices (\d+) (\d+)$";  					--regular expression to parse first line 
   begin
-    Get_Line (Input_File, Buffer, Last);
+    Get_Line (Input_File, Buffer, Last);												--reads first line of Input_File
     Match (Compile (Vertices_Regexp), Buffer (1 .. Last), Matches);
-    if Matches (0) = No_Match then
+    if Matches (0) = No_Match then														--exits if first line does not match *Vertices # #
       Put_Line ("*Vertices line was not found.");
       return;
     end if;
-    N_Vertices := Positive'Value (Buffer (Matches (1).First .. Matches (1).Last));
-    N_Characters := Positive'Value (Buffer (Matches (2).First .. Matches (2).Last));
-  end;
-  Put_Line ("Number of vertices = " & Positive'Image (N_Vertices));
-  Put_Line ("Number of characters = " & Positive'Image (N_Characters));
-  if N_Characters > N_Vertices then
+    N_Vertices := Positive'Value (Buffer (Matches (1).First .. Matches (1).Last)); 		--set value to number of vertices (character + comic books) from first line
+    N_Characters := Positive'Value (Buffer (Matches (2).First .. Matches (2).Last)); 	--set value to number of character from first line
+  end; --end sub-procedure
+  
+  Put_Line ("Number of vertices = " & Positive'Image (N_Vertices));						--displays number of vertices (character + comic books) 
+  Put_Line ("Number of characters = " & Positive'Image (N_Characters));					--displays number of character
+  --error checking for number of vertices (character + comic books) to be less than number of character
+  if N_Characters > N_Vertices then 
     Put_Line ("The number of characters must not exceed the number of vertices.");
     return;
   end if;
@@ -128,58 +146,64 @@ begin
   Edges.all := (others => (others => False));
 
   -- Read and parse the vertex name lines and build the array of vertex names.
+  --sub-procedure
   declare
     Matches : Match_Array (0 .. 2);
-    Vertex_Regexp : constant String := "^(\d+) \""(.*)\""$";
-    Vertex_Pattern_Matcher : Pattern_Matcher := Compile (Vertex_Regexp);
-  begin
+    Vertex_Regexp : constant String := "^(\d+) \""(.*)\""$";				--regular expression to parse a line (keeping name only)
+    Vertex_Pattern_Matcher : Pattern_Matcher := Compile (Vertex_Regexp);	--compiles the regular expression
+  begin 
     Put_Line ("Reading the vertex names.");
-    for I in Vertex_Names'range loop
-      Get_Line (Input_File, Buffer, Last);
-      Match (Vertex_Pattern_Matcher, Buffer (1 .. Last), Matches);
-      if Matches (0) = No_Match then
+    for I in Vertex_Names'range loop										--loops through all the characters and comic books (N_Vertices)
+      Get_Line (Input_File, Buffer, Last);									--read line from Input_File move into buffer
+      Match (Vertex_Pattern_Matcher, Buffer (1 .. Last), Matches);			--parse buffer using regular expression
+      if Matches (0) = No_Match then										--error checking (did not match regular expression requirement)
         Put_Line (Buffer (1 .. Last));
         Put_Line ("Failed to match vertex name regular expression.");
         return;
       end if;
-      Vertex_Names (I) := new String'(Buffer (Matches (2).First .. Matches (2).Last));
+      Vertex_Names (I) := new String'(Buffer (Matches (2).First .. Matches (2).Last));	--adds buffer to Vertex_Names (array of strings)
     end loop;
-  end;
+  end; --end sub-procedure
 
 --  for I in Vertex_Names'Range loop
 --    Put_Line (Positive'Image (I) & " : " & Vertex_Names (I).all);
 --  end loop;
 
   -- Read and parse the (trivial) *Edgeslist line.
+  --sub-procedure
+  --Note: does nothing unless *Edgeslist line isn't there
   declare
     Matches : Match_Array (0 .. 0);
-    Edgeslist_Regexp : constant String := "^\*Edgeslist$";
+    Edgeslist_Regexp : constant String := "^\*Edgeslist$";			--regular expression to parse *Edgeslist line
   begin
-    Get_Line (Input_File, Buffer, Last);
+    Get_Line (Input_File, Buffer, Last);							--read line from Input_File move into buffer
     Match (Compile (Edgeslist_Regexp), Buffer (1 .. Last), Matches);
     if Matches (0) = No_Match then
       Put_Line ("*Edgelist line not found.");
       return;
     end if;
-  end;
+  end; --end sub-procedure
 
   -- Read and parse the edge lines and build the edge matrix.
   -- Since these are just space separated integers, we can use the standard
   -- IO routines. Note that we also specified the valid ranges for the Source
   -- and Target variables.
+  --sub-procedure
   declare
-    Source : Positive range 1 .. N_Characters;
-    Target : Positive range N_Characters + 1 .. N_Vertices;
+    --Characters
+    Source : Positive range 1 .. N_Characters;					--declaring an integer in the range of (1 to the number of characters) 
+	--Comic books
+    Target : Positive range N_Characters + 1 .. N_Vertices;		--declaring an integer in the range of (number of characters to the number vertices) 
   begin
     Put_Line ("Reading the edge matrix.");
-    while not End_Of_File (Input_File) loop
+    while not End_Of_File (Input_File) loop						--loop through remaining lines
       Get (Input_File, Source);
-      while not End_Of_line (Input_File) loop
+      while not End_Of_line (Input_File) loop					--loops through each line individually
         Get (Input_File, Target);
-        Edges (Source, Target) := True;
+        Edges (Source, Target) := True;							--sets the matrix location to true (character, comic book)
       end loop;
     end loop;
-  end;
+  end; --end sub-procedure
 
 --  for I in Edges'Range(1) loop
 --    Put (Positive'Image (I));
@@ -204,47 +228,25 @@ begin
 
   -- Gather the character and comic book statistics and print them.
   declare
-    Character_Counts : Count_Vector (Edges'Range (1)) := (others => 0);
-    Comic_Book_Counts : Count_Vector (Edges'Range (2)) := (others => 0);
+    Character_Counts : Count_Vector (Edges'Range (1)) := (others => 0);		--declares a vector of positive numbers with the range of (edge's column size)
+    Comic_Book_Counts : Count_Vector (Edges'Range (2)) := (others => 0);	--declares a vector of positive numbers with the range of (edge's row size)
   begin
-    for I in Edges'Range (1) loop
-      for J in Edges'Range (2) loop
-        if Edges (I, J) then
-          Character_Counts (I) := Character_Counts (I) + 1;
-          Comic_Book_Counts (J) := Comic_Book_Counts (J) + 1;
+    for I in Edges'Range (1) loop											--loops through every character
+      for J in Edges'Range (2) loop											--loops through every comic book
+        if Edges (I, J) then												--if the character was in the comic book (Edges (character, comic book) is true) then...
+          Character_Counts (I) := Character_Counts (I) + 1;					--increments Character_Counts (number of comic books character(I) has been in) 
+          Comic_Book_Counts (J) := Comic_Book_Counts (J) + 1;				--increments Comic_Book_Counts (number of characters in comic books(I)) 
         end if;
       end loop;
     end loop;
-    Print_Statistics (Character_Counts, "comic books per character");
-    Print_Statistics (Comic_Book_Counts, "characters per comic book");
-  end;
+    Print_Statistics (Character_Counts, "comic books per character");		--calls Print_Statistics for character
+    Print_Statistics (Comic_Book_Counts, "characters per comic book");		--calls Print_Statistics for comic books
+  end; --end sub-procedure
 
   -- Your code to do the other processing will go here.
 
   -- Here is the code for "Total Number of Collaborations"
 
-  --Edges := new Edge_Matrix (1 .. N_Characters, N_Characters + 1 .. N_Vertices);
-  --Edges.all := (others => (others => False));
-
-  -- Here is where I will create and initialize the collaboration matrix to keep
-  -- everything organized. by Kyle Ryan
-  --declare
-    Collaborations := new Collaboration_Matrix (1 .. N_Characters, 1 .. N_Characters);
-    Collaborations.all := (others => (others => 0));
-
-  --code below by: Eric Nguyen
-  --begin
-    for j in Integer range 6487..19428 loop
-      for i in Integer range 1 .. N_Characters loop
-        if Edges (i, j) = Edges ( i+1 , j) then
-          if Edges (i, j) = true then
-            Collaborations (i, i+1) := Collaborations (i, i+1) + 1;
-          end if;
-        end if;
-      end loop;
-    end loop;
-
-  --end;
 
 
 
@@ -265,4 +267,5 @@ begin
 
 
 end Marvel_Universe_Hint;
+
 
